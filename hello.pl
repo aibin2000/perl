d3b7ba97ec9b58d931d33a9afbb13f4183ae3ce8
Peter Ai <Peter.Ai@optus.com.au>
2023-03-13 03:02:52 +1100

    init

---
+++ hello.pl
@@ -0,0 +1,18 @@
+# perl comment
+$my_name = "2";
+print("Hello and welcome " . $my_name .". Peter!\n");
+
+$s1 = 'my name is "peter ai"';
+print $s1 ."\n";
+
+$s1 = "my name is 'peter ai'\n";
+print $s1;
+
+$s2 = "my name is \"peter ai\"\n";
+print $s2;
+
+$multi = <<EOL;
+What's going to happen is that
+nothing is free lunch. You have to pay for it, Sir! Hi ooo
+EOL
+print $multi ."\n";
